c1062d427c1ab721a4726c67a603298a9150ba69
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-12-05 11:43:18 +0100

    Add proper wrapper for the API

--- src/api/server_api.cpp
+++ src/api/server_api.cpp
@@ -82,87 +82,107 @@ void retirement_configure_api(const httplib::Request& req, httplib::Response& re
     api_success(req, res, "Retirement configuration was saved");
 }
 
+auto api_wrapper(void (*api_function)(const httplib::Request &, httplib::Response &)) {
+    return [api_function](const httplib::Request& req, httplib::Response& res) {
+        try {
+            api_function(req, res);
+        } catch (const budget_exception& e) {
+            res.set_content("Exception occured: " + e.message(), "text/plain");
+            std::cout << "ERROR: budget_exception occured in api("
+                      << req.path << "): " << e.message() << std::endl;
+        } catch (const date_exception& e) {
+            res.set_content("Exception occured: " + e.message(), "text/plain");
+            std::cout << "ERROR: date_exception occured in api("
+                      << req.path << "): " << e.message() << std::endl;
+        } catch (...) {
+            res.set_content("Unkown Exception occured", "text/plain");
+            std::cout << "ERROR: Unknown exception occured in api("
+                      << req.path << ")" << std::endl;
+        }
+    };
+}
+
 } //end of anonymous namespace
 
 void budget::load_api(httplib::Server& server) {
-    server.Get("/api/server/up/", &server_up_api);
-    server.Get("/api/server/version/", &server_version_api);
-    server.Post("/api/server/version/support/", &server_version_support_api);
+    server.Get("/api/server/up/", api_wrapper(&server_up_api));
+    server.Get("/api/server/version/", api_wrapper(&server_version_api));
+    server.Post("/api/server/version/support/", api_wrapper(&server_version_support_api));
 
-    server.Post("/api/accounts/add/", &add_accounts_api);
-    server.Post("/api/accounts/edit/", &edit_accounts_api);
-    server.Get("/api/accounts/delete/", &delete_accounts_api);
-    server.Post("/api/accounts/archive/month/", &archive_accounts_month_api);
-    server.Post("/api/accounts/archive/year/", &archive_accounts_year_api);
-    server.Get("/api/accounts/list/", &list_accounts_api);
+    server.Post("/api/accounts/add/", api_wrapper(&add_accounts_api));
+    server.Post("/api/accounts/edit/", api_wrapper(&edit_accounts_api));
+    server.Get("/api/accounts/delete/", api_wrapper(&delete_accounts_api));
+    server.Post("/api/accounts/archive/month/", api_wrapper(&archive_accounts_month_api));
+    server.Post("/api/accounts/archive/year/", api_wrapper(&archive_accounts_year_api));
+    server.Get("/api/accounts/list/", api_wrapper(&list_accounts_api));
 
-    server.Post("/api/incomes/add/", &add_incomes_api);
-    server.Post("/api/incomes/edit/", &edit_incomes_api);
-    server.Get("/api/incomes/delete/", &delete_incomes_api);
-    server.Get("/api/incomes/list/", &list_incomes_api);
+    server.Post("/api/incomes/add/", api_wrapper(&add_incomes_api));
+    server.Post("/api/incomes/edit/", api_wrapper(&edit_incomes_api));
+    server.Get("/api/incomes/delete/", api_wrapper(&delete_incomes_api));
+    server.Get("/api/incomes/list/", api_wrapper(&list_incomes_api));
 
-    server.Post("/api/expenses/add/", &add_expenses_api);
-    server.Post("/api/expenses/edit/", &edit_expenses_api);
-    server.Get("/api/expenses/delete/", &delete_expenses_api);
-    server.Get("/api/expenses/list/", &list_expenses_api);
+    server.Post("/api/expenses/add/", api_wrapper(&add_expenses_api));
+    server.Post("/api/expenses/edit/", api_wrapper(&edit_expenses_api));
+    server.Get("/api/expenses/delete/", api_wrapper(&delete_expenses_api));
+    server.Get("/api/expenses/list/", api_wrapper(&list_expenses_api));
 
-    server.Post("/api/earnings/add/", &add_earnings_api);
-    server.Post("/api/earnings/edit/", &edit_earnings_api);
-    server.Get("/api/earnings/delete/", &delete_earnings_api);
-    server.Get("/api/earnings/list/", &list_earnings_api);
+    server.Post("/api/earnings/add/", api_wrapper(&add_earnings_api));
+    server.Post("/api/earnings/edit/", api_wrapper(&edit_earnings_api));
+    server.Get("/api/earnings/delete/", api_wrapper(&delete_earnings_api));
+    server.Get("/api/earnings/list/", api_wrapper(&list_earnings_api));
 
-    server.Post("/api/recurrings/add/", &add_recurrings_api);
-    server.Post("/api/recurrings/edit/", &edit_recurrings_api);
-    server.Get("/api/recurrings/delete/", &delete_recurrings_api);
-    server.Get("/api/recurrings/list/", &list_recurrings_api);
+    server.Post("/api/recurrings/add/", api_wrapper(&add_recurrings_api));
+    server.Post("/api/recurrings/edit/", api_wrapper(&edit_recurrings_api));
+    server.Get("/api/recurrings/delete/", api_wrapper(&delete_recurrings_api));
+    server.Get("/api/recurrings/list/", api_wrapper(&list_recurrings_api));
 
-    server.Post("/api/debts/add/", &add_debts_api);
-    server.Post("/api/debts/edit/", &edit_debts_api);
-    server.Get("/api/debts/delete/", &delete_debts_api);
-    server.Get("/api/debts/list/", &list_debts_api);
+    server.Post("/api/debts/add/", api_wrapper(&add_debts_api));
+    server.Post("/api/debts/edit/", api_wrapper(&edit_debts_api));
+    server.Get("/api/debts/delete/", api_wrapper(&delete_debts_api));
+    server.Get("/api/debts/list/", api_wrapper(&list_debts_api));
 
-    server.Post("/api/fortunes/add/", &add_fortunes_api);
-    server.Post("/api/fortunes/edit/", &edit_fortunes_api);
-    server.Get("/api/fortunes/delete/", &delete_fortunes_api);
-    server.Get("/api/fortunes/list/", &list_fortunes_api);
+    server.Post("/api/fortunes/add/", api_wrapper(&add_fortunes_api));
+    server.Post("/api/fortunes/edit/", api_wrapper(&edit_fortunes_api));
+    server.Get("/api/fortunes/delete/", api_wrapper(&delete_fortunes_api));
+    server.Get("/api/fortunes/list/", api_wrapper(&list_fortunes_api));
 
-    server.Post("/api/wishes/add/", &add_wishes_api);
-    server.Post("/api/wishes/edit/", &edit_wishes_api);
-    server.Get("/api/wishes/delete/", &delete_wishes_api);
-    server.Get("/api/wishes/list/", &list_wishes_api);
+    server.Post("/api/wishes/add/", api_wrapper(&add_wishes_api));
+    server.Post("/api/wishes/edit/", api_wrapper(&edit_wishes_api));
+    server.Get("/api/wishes/delete/", api_wrapper(&delete_wishes_api));
+    server.Get("/api/wishes/list/", api_wrapper(&list_wishes_api));
 
-    server.Post("/api/assets/add/", &add_assets_api);
-    server.Post("/api/assets/edit/", &edit_assets_api);
-    server.Get("/api/assets/delete/", &delete_assets_api);
-    server.Get("/api/assets/list/", &list_assets_api);
+    server.Post("/api/assets/add/", api_wrapper(&add_assets_api));
+    server.Post("/api/assets/edit/", api_wrapper(&edit_assets_api));
+    server.Get("/api/assets/delete/", api_wrapper(&delete_assets_api));
+    server.Get("/api/assets/list/", api_wrapper(&list_assets_api));
 
-    server.Post("/api/asset_values/add/", &add_asset_values_api);
-    server.Post("/api/asset_values/edit/", &edit_asset_values_api);
-    server.Post("/api/asset_values/batch/", &batch_asset_values_api);
-    server.Get("/api/asset_values/delete/", &delete_asset_values_api);
-    server.Get("/api/asset_values/list/", &list_asset_values_api);
+    server.Post("/api/asset_values/add/", api_wrapper(&add_asset_values_api));
+    server.Post("/api/asset_values/edit/", api_wrapper(&edit_asset_values_api));
+    server.Post("/api/asset_values/batch/", api_wrapper(&batch_asset_values_api));
+    server.Get("/api/asset_values/delete/", api_wrapper(&delete_asset_values_api));
+    server.Get("/api/asset_values/list/", api_wrapper(&list_asset_values_api));
 
-    server.Post("/api/asset_shares/add/", &add_asset_shares_api);
-    server.Post("/api/asset_shares/edit/", &edit_asset_shares_api);
-    server.Get("/api/asset_shares/delete/", &delete_asset_shares_api);
-    server.Get("/api/asset_shares/list/", &list_asset_shares_api);
+    server.Post("/api/asset_shares/add/", api_wrapper(&add_asset_shares_api));
+    server.Post("/api/asset_shares/edit/", api_wrapper(&edit_asset_shares_api));
+    server.Get("/api/asset_shares/delete/", api_wrapper(&delete_asset_shares_api));
+    server.Get("/api/asset_shares/list/", api_wrapper(&list_asset_shares_api));
 
-    server.Post("/api/asset_classes/add/", &add_asset_classes_api);
-    server.Post("/api/asset_classes/edit/", &edit_asset_classes_api);
-    server.Get("/api/asset_classes/delete/", &delete_asset_classes_api);
-    server.Get("/api/asset_classes/list/", &list_asset_classes_api);
+    server.Post("/api/asset_classes/add/", api_wrapper(&add_asset_classes_api));
+    server.Post("/api/asset_classes/edit/", api_wrapper(&edit_asset_classes_api));
+    server.Get("/api/asset_classes/delete/", api_wrapper(&delete_asset_classes_api));
+    server.Get("/api/asset_classes/list/", api_wrapper(&list_asset_classes_api));
 
-    server.Post("/api/liabilities/add/", &add_liabilities_api);
-    server.Post("/api/liabilities/edit/", &edit_liabilities_api);
-    server.Get("/api/liabilities/delete/", &delete_liabilities_api);
-    server.Get("/api/liabilities/list/", &list_liabilities_api);
+    server.Post("/api/liabilities/add/", api_wrapper(&add_liabilities_api));
+    server.Post("/api/liabilities/edit/", api_wrapper(&edit_liabilities_api));
+    server.Get("/api/liabilities/delete/", api_wrapper(&delete_liabilities_api));
+    server.Get("/api/liabilities/list/", api_wrapper(&list_liabilities_api));
 
-    server.Post("/api/retirement/configure/", &retirement_configure_api);
+    server.Post("/api/retirement/configure/", api_wrapper(&retirement_configure_api));
 
-    server.Post("/api/objectives/add/", &add_objectives_api);
-    server.Post("/api/objectives/edit/", &edit_objectives_api);
-    server.Get("/api/objectives/delete/", &delete_objectives_api);
-    server.Get("/api/objectives/list/", &list_objectives_api);
+    server.Post("/api/objectives/add/", api_wrapper(&add_objectives_api));
+    server.Post("/api/objectives/edit/", api_wrapper(&edit_objectives_api));
+    server.Get("/api/objectives/delete/", api_wrapper(&delete_objectives_api));
+    server.Get("/api/objectives/list/", api_wrapper(&list_objectives_api));
 }
 
 bool budget::api_start(const httplib::Request& req, httplib::Response& res) {
