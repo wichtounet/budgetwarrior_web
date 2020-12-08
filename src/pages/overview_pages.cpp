396a5eb22a6f951de92a51d2fd96ae4036a64e01
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-12-08 16:09:22 +0100

    Refactorings

--- src/pages/overview_pages.cpp
+++ src/pages/overview_pages.cpp
@@ -361,6 +361,74 @@ void budget::time_graph_tax_rate_page(html_writer & w) {
     w << "Taxes support not configured";
 }
 
+namespace {
+
+void display_side_month_overview(budget::month month, budget::year year, budget::writer& writer){
+    auto accounts = all_accounts(writer.cache, year, month);
+
+    writer << title_begin << "Side Hustle Overview of " << month << " " << year << budget::year_month_selector{"side_hustle/overview", year, month} << title_end;
+
+    auto side_category = config_value("side_category");
+    auto side_prefix   = config_value("side_prefix");
+
+    std::vector<std::vector<std::string>> contents;
+    std::vector<money> total_expenses(1, budget::money());
+    std::vector<money> total_earnings(1, budget::money());
+
+    std::vector<std::string> columns = {side_category};
+    std::unordered_map<std::string, size_t> indexes = {{side_category, 0}};
+
+    std::vector<budget::expense> side_expenses;
+    std::vector<budget::earning> side_earnings;
+
+    for (auto& expense : writer.cache.expenses()) {
+        if (get_account(expense.account).name == side_category) {
+            if (expense.name.find(side_prefix) == 0) {
+                side_expenses.push_back(expense);
+            }
+        }
+    }
+
+    for (auto& earning : writer.cache.earnings()) {
+        if (get_account(earning.account).name == side_category) {
+            if (earning.name.find(side_prefix) == 0) {
+                side_earnings.push_back(earning);
+            }
+        }
+    }
+
+    //Expenses
+    add_expenses_column(month, year, "Expenses", contents, indexes, columns.size(), side_expenses, total_expenses);
+
+    //Earnings
+    contents.emplace_back(columns.size() * 3, "");
+    add_earnings_column(month, year, "Earnings", contents, indexes, columns.size(), side_earnings, total_earnings);
+
+    writer.display_table(columns, contents, 3);
+
+    auto income = total_earnings[0];
+    auto total_all_expenses = total_expenses[0];
+
+    budget::money savings = income - total_all_expenses;
+    double savings_rate = 0.0;
+
+    if (savings.value > 0) {
+        savings_rate = 100 * (savings / income);
+    }
+
+    std::vector<std::string> second_columns;
+    std::vector<std::vector<std::string>> second_contents;
+
+    second_contents.emplace_back(std::vector<std::string>{"Total expenses", budget::to_string(total_all_expenses)});
+    second_contents.emplace_back(std::vector<std::string>{"Total earnings", budget::to_string(income)});
+    second_contents.emplace_back(std::vector<std::string>{"Savings", budget::to_string(savings)});
+    second_contents.emplace_back(std::vector<std::string>{"Savings Rate", budget::to_string(savings_rate) + "%"});
+
+    writer.display_table(second_columns, second_contents, 1, {}, accounts.size() * 9 + 1);
+}
+
+} // end of anonymous namespace
+
 void budget::side_overview_page(html_writer & w, const httplib::Request& req) {
     if(config_value("side_category", "").empty() || config_value("side_prefix", "").empty()) {
         w << "Side hustle is not configured";
