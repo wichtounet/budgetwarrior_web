cb01aec0e720dd960a3867891648607f0195fb2f
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-12-28 17:09:35 +0100

    Update highcharts

--- src/pages/html_writer.cpp
+++ src/pages/html_writer.cpp
@@ -647,10 +647,10 @@ void budget::html_writer::load_deferred_scripts(){
     // Highcharts
     if (need_module("highcharts")) {
         os << R"=====(
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/6.0.4/highstock.js" integrity="sha256-ZdoT00QjMb+DdcNmKfZJYcZY6/H83RYoLS4sZRL43T8=" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/6.0.4/highcharts-more.js" integrity="sha256-QnoLQZe7BYRVTl3AY8Lsw6mn60HfHZNpcZBEndybfBk=" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/6.0.4/js/modules/solid-gauge.js" integrity="sha256-AIfWX+axQ036B1bKbqeWxklZ4BILxbfcNKDh+sqFS+g=" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/6.0.4/js/modules/series-label.js" integrity="sha256-58Ca6fVLKQfXdNwnmkcPq09InNJa/Io8EJPJtKXT70g=" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/8.2.2/highstock.min.js" integrity="sha512-19gAF9HNSqLKHgzfGsJEvcjlHn706XrfGX5LSEAGX8y+b1Ny6tzvwL8/ZDASPH7x/XzUIQCfYFJym2yLB/mYRQ==" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/8.2.2/highcharts-more.min.js" integrity="sha512-lElibVUUvKbyo7Y1XaQRIfVHCSL3vHEotgH5pSKxVVmcSYVc1OTQgiuAi3kK1D4fNJS/+6L+mx2ZrBrDmp8vOg==" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/8.2.2/modules/solid-gauge.min.js" integrity="sha512-UnTpkIn4SRhVVIrhDUw4BFfneaLRPvw2zS2um0JfMTSex8P5GEjCEDg8+5rdcFh1/alQHDdwtQezMktVBBU7ew==" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/8.2.2/modules/series-label.min.js" integrity="sha512-ppfBb+ms2RL9FloNCD8c9mykruT6sI5ACsAdSgS69pDv/Dnn64LGewhmLnMVTCm3Zt5K2LXamZD4Ndr9EFUhMQ==" crossorigin="anonymous"></script>
         )=====";
     }
 
