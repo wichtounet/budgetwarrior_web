fcea5341bb8840e50ab99dd7c1826e09d3ed4437
Baptiste Wicht <baptiste.wicht@gmail.com>
2023-08-16 14:02:38 +0200

    Fix bug with modules

--- src/pages/html_writer.cpp
+++ src/pages/html_writer.cpp
@@ -697,7 +697,7 @@ void budget::html_writer::load_deferred_scripts(){
 }
 
 void budget::html_writer::use_module(const std::string& module){
-    if (std::ranges::contains(modules, module)) {
+    if (!std::ranges::contains(modules, module)) {
         modules.push_back(module);
     }
 }
